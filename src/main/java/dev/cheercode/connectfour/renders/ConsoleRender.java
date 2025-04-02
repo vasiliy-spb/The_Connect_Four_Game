0381ec58991edcd6c930afca044f5382c1a8910a
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-04-02 22:07:27 +0700

    Refactor of ConsoleRender

--- src/main/java/dev/cheercode/connectfour/renders/ConsoleRender.java
+++ src/main/java/dev/cheercode/connectfour/renders/ConsoleRender.java
@@ -6,17 +6,27 @@ public class ConsoleRender implements Render {
     private static final String BLUE_DISK = "\uD83D\uDD35";
     private static final String RED_DISK = "\uD83D\uDD34";
     private static final String EMPTY_DISK = "⚪";
+    private static final String SPACE = " ";
 
     @Override
     public void display(Disk[][] grid) {
-        for (int i = 1; i <= grid[0].length; i++) {
-            System.out.print(" " + i + " ");
+        int width = grid[0].length;
+        printColumnNumbers(width);
+        printGrid(grid);
+    }
+
+    private static void printColumnNumbers(int width) {
+        for (int i = 1; i <= width; i++) {
+            System.out.print(SPACE + i + SPACE);
         }
         System.out.println();
+    }
+
+    private void printGrid(Disk[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 String representation = getRepresentation(grid[i][j]);
-                System.out.print(representation + " ");
+                System.out.print(representation + SPACE);
             }
             System.out.println();
         }
